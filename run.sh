d8d3a339bf4ddde19ac843b498b0d2f377f4d9c7
Mark Schmitt <31215086+MarkSchmitt@users.noreply.github.com>
2021-01-29 08:55:28 +0100

    comments: added more explaination

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+# /run/user/ will pass through some socket files, especiall for dbus, and /tmp passes through access to x11. DISPLAY makes sure your clients knows where to connect to
 docker run --rm -it --user `id -u` -v /run/user/`id -u`:/run/user/`id -u` -e DISPLAY=$DISPLAY -h ubuntu-x11-in-docker -v /home/`whoami`:/home/`whoami` -v /tmp:/tmp --workdir `pwd` ubuntu-x11-in-docker /bin/bash
